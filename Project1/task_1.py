7c888d1d6d5e91c8da2c5baf3ef5549cfc38e691
Anja Rosvold From <anjarfrom@gmail.com>
2019-03-11 16:56:04 +0100

    Remove if name = main

--- Project1/task_1.py
+++ Project1/task_1.py
@@ -1,5 +1,4 @@
 from rdd import RDD
 
-if __name__ == '__main__':
-    count = RDD("./datasets/albums.csv").map(lambda line: line.split(",")[3]).distinct().count()
-    print("There are {} distinct genres in albums.csv.".format(count))
+count = RDD("./datasets/albums.csv").map(lambda line: line.split(",")[3]).distinct().count()
+print("There are {} distinct genres in albums.csv.".format(count))
