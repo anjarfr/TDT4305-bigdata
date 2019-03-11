0bb9cfcdcfd31d9fa996b5a60daa463e57ebca30
sigurdeileras <sigurd.eileras@gmail.com>
2019-03-11 11:52:22 +0100

    task 8 completed

---
+++ Project1/task_8.py
@@ -0,0 +1,17 @@
+from rdd import RDD, toTSVLine
+
+def find_name(line):
+    if line[2]:
+        return line[2]
+    else:
+        return line[1]
+
+
+albums = RDD('./datasets/albums.csv').map(lambda line: line.split(',')).map(lambda col: (col[1], col[8])).filter(lambda album: album[1] == "5")
+
+artists = RDD('./datasets/artists.csv').map(lambda line: line.split(',')).map(lambda col: (col[0], find_name(col)))
+
+artists_MTV_5 = albums.join(artists).map(lambda line: line[1][1]).distinct().sortBy(lambda x: x[0], ascending=True)
+
+artists_MTV_5.map(lambda x: '{name}'.format(name=x.encode('utf-8'))).saveAsTextFile("./datasets/result_8")
+
