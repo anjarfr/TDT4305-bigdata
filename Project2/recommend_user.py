b8991a23411827d9217ca6fede098fab240a04f2
Anja Rosvold From <anjarfrom@gmail.com>
2019-03-27 15:58:44 +0100

    Less overhead

--- Project2/recommend_user.py
+++ Project2/recommend_user.py
@@ -17,32 +17,28 @@ def RDD(filePath):
     return rdd
 
 
-def compare(user_list, x):
-    x_list = x[1]
-    count = 0
-    for word in user_list:
-        if word in x_list:
-            count += 1
-            x_list.remove(word)
-    new_x = (x[0], count)
-    return new_x
-
-
 def counter(user_name, rdd):
+
+    def compare(user_list, x):
+        x_list = x[1]
+        count = 0
+        for word in user_list:
+            if word in x_list:
+                count += 1
+                x_list.remove(word)
+        new_x = (x[0], count)
+        return new_x
+
     user_list = rdd.lookup(user_name)[0]
     similar = rdd.map(lambda x: compare(user_list, x))
     return similar
 
 
-def sort(rdd):
-    sorted = rdd.sortBy(lambda x: x[1])
-    return sorted
-
 # Metoden.
 def recommend_user(user_name, k, file_path, output_path):
     rdd = RDD(file_path)
     counted = counter(user_name=user_name, rdd=rdd)
-    sorted = sort(counted)
+    sorted = counted.sortBy(lambda x: x[1])
     recommendation = sorted.take(k)
     recommendation.map(lambda x: '{username}\t{count}'.format(username=x[0], count=x[1])).saveAsTextFile(output_path)
 
